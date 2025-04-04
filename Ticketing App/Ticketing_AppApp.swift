c9cbfeceee4e7ffc6750d16f6b4a98d20de14745
nikhil <nikhil@innobles.com>
2025-04-04 11:42:48 +0530

    first commit

---
+++ Ticketing App/Ticketing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ticketing_AppApp.swift
+//  Ticketing App
+//
+//  Created by nikhil tiwari on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Ticketing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ticketing AppTests/Ticketing_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ticketing_AppTests.swift
+//  Ticketing AppTests
+//
+//  Created by nikhil tiwari on 04/04/25.
+//
+
+import Testing
+@testable import Ticketing_App
+
+struct Ticketing_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
